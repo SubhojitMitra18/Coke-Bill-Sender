f12b7bd7df2769524e26f82a5bdf2a88a5628d50
Subhojit mitra <132591436+SubhojitMitra18@users.noreply.github.com>
2024-08-15 23:28:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static('public'));
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
